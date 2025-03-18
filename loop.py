1f39687c40ad2c0e823fbd5567085de9510055b8
mavoo-a11y <marvinlone456@gmail.com>
2025-03-18 18:09:02 +0300

    loop.py

---
+++ loop.py
@@ -0,0 +1,54 @@
+value = 1
+#  while value <= 10:
+#    print(value)
+#   if value == 5:
+#      break      
+#   value += 1 
+
+#  while value <= 10:
+#     value += 1
+#    if value == 5:
+#        continue
+#    print(value)
+#  else:
+#   print("value is now equal to " + str(value))
+
+names =("Marcus","Charles","Marvin")
+#  for x in names:
+#    print(x)
+
+#     for x in "Nairobi":
+#          print(x)
+
+#  for x in names:
+#     if x == "Charles":
+#          break
+#      print(x)
+
+#  for x in names:
+#      if x == "Charles":
+#         break
+#     print(x)
+
+#  for x in range(4):
+#      print(x)
+
+for x in range(2,4):
+     print(x)
+
+for x in range(5,101,5):
+     print(x)
+else:
+     print("Glad that\'s over!")
+     
+     names = ("Marcus,""Charles","Marvin")
+     actions = ("codes,""eats","sleeps")
+
+    #  for name in names:
+    #       for action in actions:
+    #            print(name + " " + action + ".")
+
+           
+     for action in names:
+          for name in names:
+               print(name + " " + action + ".")    
