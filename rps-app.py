64f6b53717f1632b8eb9e4e46d754a5febc0776e
mavoo-a11y <marvinlone456@gmail.com>
2025-03-25 20:06:57 +0300

    rps-app.py

---
+++ rps-app.py
@@ -0,0 +1,10 @@
+name = input("What is your name?:")
+age = int (input("How old are you?:"))
+weight = input("How much do you weigh?:")
+height = input("How tall are you?:")
+
+
+print("Hello " +name)
+print("You are "+str(age)+" years old")
+print("You are "+str(weight)+"in kg")
+print("You are "+str(height)+"cm tall")
